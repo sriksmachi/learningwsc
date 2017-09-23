cdadced98a0841eb4330719c487a04aed1d4fe7e
srikanth <srikanth@WSC-DEV-1016>
2017-09-23 12:06:29 +0000

    aspnet core iis image pushed

---
+++ chapter4/completed/aspnetcore-iis/musicstore/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Server;
+using System.IO;
+
+namespace MusicStore
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseUrls()
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
